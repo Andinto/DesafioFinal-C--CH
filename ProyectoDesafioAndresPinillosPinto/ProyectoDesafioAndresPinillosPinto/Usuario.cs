324f6d7ae5d0cbb13958de272f317dfc18406e26
Andres Pinillos Pinto <animalarggamer@hotmail.com>
2024-09-04 20:34:38 -0300

    Se sube Desafio N1
    Se crearon las clases Entidades y sus propiedades

---
+++ ProyectoDesafioAndresPinillosPinto/ProyectoDesafioAndresPinillosPinto/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDesafioAndresPinillosPinto
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string nombreUsuario { get; set; }
+        public string contraseña { get; set; }
+        public string mail { get; set; }
+    }
+
+}
